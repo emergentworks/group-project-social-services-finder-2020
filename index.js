a7cf4f8fc1c7a6fc1abae067eaf8623b0da67e2e
rion.davis82@gmail.com <rion.davis82@gmail.com>
2020-02-15 16:02:41 -0500

    edits to search bar functions and styling

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function closeToast() {
   document.querySelector('.toast').classList.add('toast--hide');
 }
+
+function searchTermFinder() {
+  const searchTerm = "jobs"
+  const searchMapper = {
+    "resume": "/not_emergency/resume.html",
+    "jobs": "/not_emergency/resume.html",
+    "apartment": "/not_emergency/apartments.html",
+  }
+  const keys = Object.keys(searchMapper)
+  console.log(keys)
+
+  for (var i = 0; i < keys.length; i++) {
+    // is this searchTerm in searchMapper?
+    // if so, return the URL
+    // navigate to that URL
+    }
+    // if (searchTerm === "resume") {
+    //   return "/not_emergency/resume.html";
+    // }
+}
